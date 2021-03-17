b96a5c6a05c2096cc753c8ee9aa5528b1bbe2071
JR Hossain.2 <jakirhossainjr.2@gmail.com>
2021-03-18 02:06:19 +0600

    Added Top Header Basic Design

---
+++ main.js
@@ -0,0 +1,6 @@
+/*
+window.addEventListener("orientationchange", function(e) {
+  console.log("the orientation of the device is now " + e.target.screen.orientation.angle);
+  console.log(e.target.screen.orientation);
+});
+*/
